c7376276e025662d6c9583b99694d1f755df55ed
minhlong1802 <137919484+minhlong1802@users.noreply.github.com>
2025-07-28 01:02:49 +0700

    add create and update discount code feature

--- src/main/java/com/example/event_ticket_system/Repository/DiscountRepository.java
+++ src/main/java/com/example/event_ticket_system/Repository/DiscountRepository.java
@@ -1,12 +1,16 @@
 package com.example.event_ticket_system.Repository;
 
 import com.example.event_ticket_system.Entity.Discount;
+import com.example.event_ticket_system.Entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DiscountRepository extends JpaRepository<Discount, Integer> {
     Optional<Discount> findByCode(String code);
+
+    List<Discount> findByEvent(Event event);
 }
